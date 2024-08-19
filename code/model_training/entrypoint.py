69f78d86227e918542c3e195018152796767d52e
blackBagel <igal.chernov@gmail.com>
2024-08-19 11:02:31 +0000

    moved dataset updater out of training dir

--- code/model_training/entrypoint.py
+++ code/model_training/entrypoint.py
@@ -4,13 +4,6 @@ from champion_trainer import train_champion
 from prefect import serve
 
 if __name__ == "__main__":
-    model_datsets_update_deployment = update_model_datsets.to_deployment(
-        name="model_datasets_updater",
-        cron="0 1 * * 1", # At 01:00 on Mondays
-        tags=["data", "scheduled"],
-        description="Updates the datasets models will be trained on",
-    )
-    
     all_models_train_deployment = retrain_all_models.to_deployment(
         name="candidate_models_train_deployment",
         cron="0 3 * * 1", # At 03:00 AM on Mondays
@@ -25,4 +18,4 @@ if __name__ == "__main__":
         description="Responsible for retraining the champion model on updated data",
     )
 
-    serve(model_datsets_update_deployment, all_models_train_deployment, champion_train_deployment)
+    serve(all_models_train_deployment, champion_train_deployment)
